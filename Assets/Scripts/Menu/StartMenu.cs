4c98af3a5d04f094881b45de433b0ab8cd7bc6a3
Deniz Sarıbayır <deniz0311@hotmail.com>
2022-02-18 13:48:36 +0300

    menu
    Start ve options menüsü yapıldı. Buton tıklama sesleri eklendi.

---
+++ Assets/Scripts/Menu/StartMenu.cs
@@ -0,0 +1,190 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject StartMenuCanvas;
+    public GameObject OptionsCanvas;
+    public GameObject CreditsCanvas;
+    public GameObject PlayCanvas;
+    public GameObject SelectCharacterCanvas;
+    public Button button30fps;
+    public Button button60fps;
+    public Button button75fps;
+    public Text gameVersion;
+    public Text volumeText;
+    public Text fpsText;
+    public Slider volumeSlider;
+    public AudioSource ClickSound;
+    public AudioClip nextClick;
+    public AudioClip backClick;
+    public static int maxFPS = 60;
+    public static float volumeValue;
+    public void Start()
+    {
+        LoadValues();
+        gameVersion.text = Application.version;
+        fpsText.text = Application.targetFrameRate.ToString();
+    }
+
+    public void PlayNextClick()
+    {
+        ClickSound.PlayOneShot(nextClick);
+    }
+
+    public void PlayBackClick()
+    {
+        ClickSound.PlayOneShot(backClick);
+    }
+
+    public void PlayButton()
+    {
+        StartMenuCanvas.SetActive(false);
+        PlayCanvas.SetActive(true);
+        PlayNextClick();
+    }
+
+    public void NewGameButton()
+    {
+        PlayCanvas.SetActive(false);
+        SelectCharacterCanvas.SetActive(true);
+        PlayNextClick();
+    }
+
+    public void ContinueButton()
+    {
+        PlayNextClick();
+    }
+
+    public void CreditsButton()
+    {
+        StartMenuCanvas.SetActive(false);
+        CreditsCanvas.SetActive(true);
+        PlayNextClick();
+    }
+    public void OptionsButton()
+    {
+        StartMenuCanvas.SetActive(false);
+        OptionsCanvas.SetActive(true);
+        PlayNextClick();
+    }
+
+    public void SaveButton()
+    {
+        volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        PlayerPrefs.SetInt("MaxFPS", maxFPS);
+        PlayerPrefs.Save();
+        LoadValues();
+        OptionsCanvas.SetActive(false);
+        StartMenuCanvas.SetActive(true);
+        PlayNextClick();
+    }
+
+    public void BackButton()
+    {
+        StartMenuCanvas.SetActive(true);
+        OptionsCanvas.SetActive(false);
+        CreditsCanvas.SetActive(false);
+        PlayCanvas.SetActive(false);
+        SelectCharacterCanvas.SetActive(false);
+        PlayBackClick();
+    }
+
+    public void QuitButton()
+    {
+        PlayNextClick();
+        Application.Quit();
+    }
+
+    public void ContinueControl()
+    {
+
+    }
+
+    public void VolumeSlider(float volume)
+    {
+        volumeText.text = (volume * 100).ToString("F0");
+    }
+
+    public void VolumeControl()
+    {
+        volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
+
+    public void Button30()
+    {
+        maxFPS = 30;
+        button30fps.interactable = false;
+        button60fps.interactable = true;
+        button75fps.interactable = true;
+        PlayNextClick();
+    }
+    public void Button60()
+    {
+        maxFPS = 60;
+        button30fps.interactable = true;
+        button60fps.interactable = false;
+        button75fps.interactable = true;
+        PlayNextClick();
+    }
+
+    public void Button75()
+    {
+        maxFPS = 75;
+        button30fps.interactable = true;
+        button60fps.interactable = true;
+        button75fps.interactable = false;
+        PlayNextClick();
+    }
+
+    public void FPSControl()
+    {
+        if (PlayerPrefs.GetInt("MaxFPS") != 0)
+        {
+            maxFPS = PlayerPrefs.GetInt("MaxFPS");
+        }
+
+        Application.targetFrameRate = maxFPS;
+        fpsText.text = Application.targetFrameRate.ToString();
+
+        if(Application.targetFrameRate == 30)
+        {
+            button30fps.interactable = false;
+            button60fps.interactable = true;
+            button75fps.interactable = true;
+        }
+        if (Application.targetFrameRate == 60)
+        {
+            button30fps.interactable = true;
+            button60fps.interactable = false;
+            button75fps.interactable = true;
+        }
+        if (Application.targetFrameRate == 75)
+        {
+            button30fps.interactable = true;
+            button60fps.interactable = true;
+            button75fps.interactable = false;
+        }
+    }
+    public void LoadValues()
+    {
+        //LoadData.loadData();
+
+        VolumeControl();
+
+        FPSControl();
+
+        ContinueControl();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
